b548dae277c6deea08981b610d0eab0b09fed87c
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2021-09-09 12:09:32 -0400

    migrate alt_text entries to core alt_text column; remove AltText module dependency

--- Module.php
+++ Module.php
@@ -3,8 +3,6 @@
 namespace AgileThemeTools;
 
 use Doctrine\ORM\Events;
-use AltText\Db\Event\Listener\DetachOrphanMappings;
-use AltText\Entity\AltText as AltTextEntity;
 use Omeka\Api\Representation\MediaRepresentation;
 use Omeka\Entity\Media as MediaEntity;
 use Omeka\Module\AbstractModule;
@@ -23,33 +21,11 @@ class Module extends AbstractModule
     {
         return include __DIR__ . '/config/module.config.php';
     }
-
-    public function onBootstrap(MvcEvent $event)
-    {
-        parent::onBootstrap($event);
-        // $acl = $this->getServiceLocator()->get('Omeka\Acl');
-        //$acl->allow(null, [\AgileThemeTools\Controller\BlockOptionsAdminController::class]);
-        $services = $this->getServiceLocator();
-        $em = $services->get('Omeka\EntityManager');
-        if (class_exists(\AltText\Db\Event\Listener\DetachOrphanMappings::class)) {
-            $em->getEventManager()->addEventListener(
-                Events::preFlush,
-                new DetachOrphanMappings
-            );
-        }
-    }
     
-    public function install(ServiceLocatorInterface $services) {
-        if (!class_exists(\AltText\Db\Event\Listener\DetachOrphanMappings::class)) {
-            $translator = $services->get('MvcTranslator');
-            $message = new \Omeka\Stdlib\Message(
-                $translator->translate('This module requires the module "%s".'), // @translate
-                'AltText'
-            );
-            throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
-        }
+    public function upgrade($oldVersion, $newVersion, ServiceLocatorInterface $serviceLocator) {
+        $conn = $serviceLocator->get('Omeka\Connection');
+        $conn->exec('UPDATE media, alt_text SET media.alt_text = alt_text.alt_text WHERE media.id = alt_text.media_id');
     }
-    
     
     // Code borrowed from the AltText module (with thanks!). Substitutes dc:description if no Alt tag provided.
     // Defaults to the title of the file, which is often just the filename.
