4e7f62044d26d55fdf4622e49b6b120f5943f805
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-09-26 10:39:31 -0400

    move thumbnail config to another custom module

--- config/module.config.php
+++ config/module.config.php
@@ -2,57 +2,6 @@
   namespace AgileThemeTools;
 
   return array(
-    // override the default thumbnail config
-    'thumbnails' => [
-        'types' => [
-            'large' => ['constraint' => 2100],
-            'medium' => ['constraint' => 800],
-            'square' => ['constraint' => 300],
-            /*
-                Custom Thumbnail Types
-                - set strategy to 'custom'
-                - set x and y constraints as array
-                - set crop (optional)
-                - example:
-                    'og_image' => [
-                        'strategy' => 'custom',
-                        'constraint' => [
-                            'constraint_x' => 1200, 
-                            'constraint_y' => 600
-                        ],
-                        'options' => [
-                            'gravity' => 'center', 
-                            'crop' => true
-                        ]
-                    ] 
-            */
-            '16x9' => [
-                'strategy' => 'custom',
-                'constraint' => [
-                    'constraint_x' => 1440, 
-                    'constraint_y' => 810
-                ],
-                'options' => [
-                    'gravity' => 'center', 
-                    'crop' => true
-                ]
-            ],
-            'og_image' => [
-                'strategy' => 'custom',
-                'constraint' => [
-                    'constraint_x' => 1200, 
-                    'constraint_y' => 630
-                ],
-                'options' => [
-                    'gravity' => 'center', 
-                    'crop' => true
-                ]
-            ]
-        ],
-        'thumbnailer_options' => [
-            'imagemagick_dir' => null,
-        ],
-    ],
       'view_manager' => [
           'template_path_stack' => [
               dirname(__DIR__) . '/view',
